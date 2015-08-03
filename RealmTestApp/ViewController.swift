7e6a245457a70b2f284adab1d566e857d80fbed5
Ryo Morimoto <morimoto@M-3.local>
2015-08-03 14:07:46 +0900

    Initial Commit

---
+++ RealmTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTestApp
+//
+//  Created by Ryo Morimoto on 2015/08/03.
+//  Copyright (c) 2015年 Ryo Morimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
